f814bb1010ae4ab2fabaefe16c226296e112e3c7
tyagi0320 <127464668+tyagi0320@users.noreply.github.com>
2024-08-10 10:33:15 +0530

    SetA-Q2

---
+++ seta++.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <cctype>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    map<char, int> frequency;
+    string paragraph;
+
+    cout << "Enter a paragraph: ";
+    getline(cin, paragraph);
+
+    for (char ch : paragraph) {
+            frequency[ch]++;
+    }
+
+   cout << "Character frequencies:" << endl;
+    for (const auto& pair : frequency) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+
+    return 0;
+}
